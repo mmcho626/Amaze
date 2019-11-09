65b23b3b34f75e43978030789cf3dd0a8a958a7a
mmcho626 <office_hagi@yahoo.co.jp>
2019-11-09 03:08:39 +0000

    コメントアウトの追加

--- db/migrate/20191108144920_create_relationships.rb
+++ db/migrate/20191108144920_create_relationships.rb
@@ -2,11 +2,13 @@ class CreateRelationships < ActiveRecord::Migration[5.2]
   def change
     create_table :relationships do |t|
 
+		#referncesは、relationshipsモデルにbelongs_to:userを自動的に追加、外部キー制約を行うマイグレーションファイルも自動的に作成
     	t.references :user, foreign_key: true
 	    t.references :follow, foreign_key: { to_table: :users }
 
 	    t.timestamps
 
+		#データの検索高速化、一意制約の付与により、重複フォローを防ぐ
 	    t.index [:user_id, :follow_id], unique: true
     end
   end
