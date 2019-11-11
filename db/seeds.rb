990ab70e2137d2b18b708d1b7afe2dcb1a301754
mmcho626 <office_hagi@yahoo.co.jp>
2019-11-11 07:56:09 +0000

    データ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+PostCategory.create!(:post_category_name=>"カテゴリー1")
+PostTime.create!(:post_time=>10)
+PostPerson.create!(:post_person=>1)
