d262b472a22229841e6d0e1eb9118693f1ef615d
mmcho626 <office_hagi@yahoo.co.jp>
2019-11-11 19:03:58 +0000

    userテーブルにnameカラム追加

--- db/migrate/20191109171840_add_column_to_users.rb
+++ db/migrate/20191109171840_add_column_to_users.rb
@@ -4,5 +4,6 @@ class AddColumnToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :user_introduction, :text
     add_column :users, :user_age, :string
     add_column :users, :user_gender, :string
+    add_column :users, :name, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 2019_11_09_171840) do
     t.text "user_introduction"
     t.string "user_age"
     t.string "user_gender"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
