a631fad9190a751916c9648f1bdfb3e2f0a95028
mmcho626 <office_hagi@yahoo.co.jp>
2019-11-12 05:40:42 +0000

    カラム変更(age, sex)

--- db/migrate/20191109171840_add_column_to_users.rb
+++ db/migrate/20191109171840_add_column_to_users.rb
@@ -2,8 +2,8 @@ class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :user_image_id, :string
     add_column :users, :user_introduction, :text
-    add_column :users, :user_age, :integer
-    add_column :users, :user_gender, :integer
+    add_column :users, :age, :integer
+    add_column :users, :sex, :integer
     add_column :users, :name, :string
   end
 end
