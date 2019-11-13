ad8088891de4db9d9fb868b108d3f880410207fa
mmcho626 <office_hagi@yahoo.co.jp>
2019-11-13 16:02:59 +0000

    schema変更

--- db/migrate/20191108144920_create_relationships.rb
+++ db/migrate/20191108144920_create_relationships.rb
@@ -1,15 +1,14 @@
 class CreateRelationships < ActiveRecord::Migration[5.2]
   def change
     create_table :relationships do |t|
+    	t.integer :follower_id
+    	t.integer :following_id
 
-		#referncesは、relationshipsモデルにbelongs_to:userを自動的に追加、外部キー制約を行うマイグレーションファイルも自動的に作成
-    	t.references :user, foreign_key: true
-	    t.references :follow, foreign_key: { to_table: :users }
-
-	    t.timestamps
-
-		#データの検索高速化、一意制約の付与により、重複フォローを防ぐ
-	    t.index [:user_id, :follow_id], unique: true
+    	t.timestamps null: false
     end
+
+    add_index :relationships, :follower_id
+    add_index :relationships, :following_id
+    add_index :relationships, [:follower_id, :following_id],unique: true
   end
 end
