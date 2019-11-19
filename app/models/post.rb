65bf720cf1667cfb62b464b41964709bc764b97d
mmcho626 <office_hagi@yahoo.co.jp>
2019-11-19 15:28:41 +0000

    Postテーブルにおけるuser_idのnil許可を解除

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
 	attachment :post1_image
 	attachment :post2_image
 
-	belongs_to :user, optional: true #一時的にnilを許可
+	belongs_to :user
 	has_many :comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
 
