b74a52152c31b06d31b9bb4973f134ced5475c03
mmcho626 <office_hagi@yahoo.co.jp>
2019-11-20 10:57:01 +0000

    管理者用サインアップ画面削除のため、ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
   }
 
   namespace :admins do
