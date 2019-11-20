7deeb2dadd52188f646f8fc9b79feb7078fa1733
mmcho626 <office_hagi@yahoo.co.jp>
2019-11-20 18:18:00 +0000

    口コミ(post)投稿時、商品(item)の名前を入力する機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,7 +32,24 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+
+    #item追加機能ここから
+
+    item = Item.search(params[:post_search_item]) #itemテーブルを参照。searchメソッドはitem.rbに記載。
+
+    if item == nil #item_nameカラムの値が空の場合はその値をitemテーブルに追加
+      @item = Item.new
+      @item.item_name = params[:post_search_item]
+      @item.save
+      @post.item_id = @item.id
+    else #値がitemテーブルに存在している場合はitem_idを登録
+      @post.item_id = item.id
+    end
+
+    #item追加機能ここまで
+
     @post.user_id = current_user.id
+
       if @post.save!
         redirect_to posts_path
       else
@@ -69,6 +86,6 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:item_id, :post1_image, :post2_image, :post_body, :star, :post_category_id, :post_person_id, :post_time_id)
+      params.require(:post).permit(:post1_image, :post2_image, :post_body, :star, :post_category_id, :post_person_id, :post_time_id)
     end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,11 @@ class Item < ApplicationRecord
 	belongs_to :item_person, optional: true #nilを許可
 	belongs_to :item_time, optional: true #nilを許可
 
+
+    # posts#create にて使用するメソッドの定義
+    # 口コミ(post)の新規投稿時、item_nameカラムの値を検索
+	def self.search(search)
+    Item.find_by(['item_name LIKE ?', "#{search}"])
+  	end
+
 end
