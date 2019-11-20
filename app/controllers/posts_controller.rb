6807ca14ffe405eed2b69299577d2e0a385b808e
mmcho626 <office_hagi@yahoo.co.jp>
2019-11-20 18:19:50 +0000

    @post.save! → @post.save に変更。エラーの検証必要なくなったため。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,7 +50,7 @@ class PostsController < ApplicationController
 
     @post.user_id = current_user.id
 
-      if @post.save!
+      if @post.save
         redirect_to posts_path
       else
         redirect_to new_post_path
