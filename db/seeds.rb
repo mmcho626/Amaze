ae16770663a113910684dd5eb31f2c40acedd535
mmcho626 <office_hagi@yahoo.co.jp>
2019-11-22 12:12:34 +0000

    seedに管理者ログイン情報追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,9 @@ ItemTime.create!(:item_time=>20)
 ItemPerson.create!(:item_person=>2)
 
 
-
 5.times do |i|
 	User.create!(:email=>"user#{i + 1}@com", :password=>"000000", :user_introduction=>"ユーザ紹介文#{i + 1}",:sex=>0, :name =>"ユーザ名", :age=>0)
 end
+
+# 管理者ログイン情報
+Admin.create(:email => "admin@com", :password=>"000000")
