bc131569e9f5a07caa685b5fef8739e265850bd2
mmcho626 <office_hagi@yahoo.co.jp>
2019-11-23 13:31:03 +0000

    不要な行削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,10 @@
 class PostsController < ApplicationController
-  # GET /posts
-  # GET /posts.json
+
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
+
   def show
     @post = Post.find(params[:id])
     @comment = Comment.new
@@ -14,12 +12,12 @@ class PostsController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # GET /posts/new
+
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
+
   def edit
     @post = Post.find(params[:id])
     if @post.user_id != current_user.id
@@ -28,8 +26,7 @@ class PostsController < ApplicationController
     end
   end
 
-  # POST /posts
-  # POST /posts.json
+
   def create
     @post = Post.new(post_params)
 
@@ -48,6 +45,7 @@ class PostsController < ApplicationController
 
     #item追加機能ここまで
 
+
     @post.user_id = current_user.id
 
       if @post.save
@@ -57,27 +55,25 @@ class PostsController < ApplicationController
       end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
+
   def update
-      @post = Post.find(params[:id])
-      if @post.update(post_params)
-        flash[:notice] = "投稿内容を更新しました"
-        redirect_to post_path(@post)
-      else
-        @post = Post.find
-        flash[:notice] = "投稿内容を更新できませんでした"
-        redirect_to edit_post_path(@post)
-      end
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "投稿内容を更新しました"
+      redirect_to post_path(@post)
+    else
+      flash[:notice] = "投稿内容を更新できませんでした"
+      redirect_to edit_post_path(@post)
+    end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
+
   def destroy
     post = Post.find(params[:id])
     post.destroy
     redirect_to posts_path
   end
+
 
   def favorites
     @post = Post.find(params[:id])
