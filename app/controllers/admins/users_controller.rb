1c6d8456e35de1179d30a2c11d5503bed2fa558f
mmcho626 <office_hagi@yahoo.co.jp>
2019-11-23 13:59:38 +0000

    user関連の管理者機能追加

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,16 +1,67 @@
 class Admins::UsersController < ApplicationController
+
   def index
+    @users = User.all
+    render 'users/index'
   end
 
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+    render 'users/show'
   end
 
   def edit
+    @user = User.find(params[:id])
+    render 'users/edit'
   end
 
   def update
+    @user = User.find(params[:id])
+    if  @user.update(user_params)
+      flash[:notice] = "保存しました"
+      redirect_to admins_user_path(@user.id)
+    else
+      flash[:notice] = "保存できませんでした"
+      redirect_to edit_admins_user_path(@user.id)
+    end
   end
 
   def destroy
   end
+
+
+  def favorites
+    @user = User.find(params[:id])
+  end
+
+  def follower
+    @user = User.find(params[:id])
+    render 'show_follow'
+  end
+
+  def followed
+    @user = User.find(params[:id])
+    render 'show_follower'
+  end
+
+
+  #検索機能
+  def search
+    @user_or_post = params[:option]
+    if @user_or_post == "1"
+      @users = User.search(params[:search], @user_or_post)
+      # Userモデルのsearchメソッドはuser.rbに記載。
+    else
+      @posts = Post.search(params[:search], @user_or_post)
+      # Postモデルのsearchメソッドはpost.rbに記載。
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :user_image, :name, :sex, :age, :user_introduction )
+  end
+
 end
